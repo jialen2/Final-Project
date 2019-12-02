bc25686c47e62072cb459017b98485b637524a56
jialening <jialen2@illinois.edu>
2019-12-01 20:45:31 -0600

    commit code

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LinearLayout welcome = findViewById(R.id)
+    }
+}
